a0bbacd5128a30ce7545d9e2ed48262db0ce54c6
RAchange <a0979730525@gmail.com>
2019-12-18 03:46:47 +0800

    new

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -22,7 +22,7 @@
       function() {
         var currentTop = $(window).scrollTop();
         //check if user is scrolling up
-        if (currentTop < this.previousTop-5) {
+        if (currentTop < this.previousTop-10) {
           //if scrolling up...
           if (currentTop > 0 && $('#mainNav').hasClass('is-fixed')) {
             $('#mainNav').addClass('is-visible');
